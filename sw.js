36f26ada440dff703227e7a370d81b4e4648b379
vpbcb <vpbcb1@gmail.com>
2026-02-10 22:19:37 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'myfuel-v4'; // Версия 4
+const CACHE_NAME = 'myfuel-v5'; // Версия 4
 const ASSETS_TO_CACHE = [
     './index.html',       // Теперь index.html
     './FuelIcon.png',
@@ -34,6 +34,7 @@ self.addEventListener('fetch', (event) => {
             .catch(() => caches.match(event.request))
     );
 });
+
 
 
 
